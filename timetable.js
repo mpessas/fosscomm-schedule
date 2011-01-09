842457bc64124165331ae15c4d71ffda268892d4
Apostolos Bessas <mpessas@gmail.com>
2011-01-09 18:13:02 +0200

    Add data through ajax

--- timetable.js
+++ timetable.js
@@ -1,4 +1,29 @@
+function get_template(row, speech) {
+    row.find('.thour').html(speech.hour);
+    row.find('.tb1').text(speech.title);
+    row.find('.tb2').text(speech.speaker);
+    return row;
+}
+
 $(function() {
+    // Fetch data to begin with
+    $.ajax({
+        type: 'GET',
+        url: 'fetch_data.json',
+        timeout: 10000,
+        dataType: 'json',
+        error: function(xhr, status, error) {
+            alert(status + ": " + error);
+        },
+        success: function(data) {
+            alert(data.length);
+            for (var i in data) {
+                var newRow = $('.template').clone().removeClass('template');
+                alert(data[i]);
+                get_template(newRow, data[i]).appendTo('table');
+            }
+        }
+    });
     // handle links to speeches
     $('td a').click(function(e) {
         // Support for js history API
