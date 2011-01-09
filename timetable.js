f150afec358c65da5e43875aa419fb2d95334d2f
Apostolos Bessas <mpessas@gmail.com>
2011-01-09 11:26:59 +0200

    Move preventDefault

--- timetable.js
+++ timetable.js
@@ -2,12 +2,12 @@
 $(function() {
     $('td a').click(function(e) {
         if (window.history && window.history.pushState) {
+            e.preventDefault()
             $('table').hide();
             var file = this.id + ".json";
             var state = { "hidden": true };
             var title = "Speech: " + this.id;
             window.history.pushState(state, title);
-            e.preventDefault()
         }
     });
 });
