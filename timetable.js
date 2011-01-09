d9b6907ed76024445d7706d54624d67c1819e609
Apostolos Bessas <mpessas@gmail.com>
2011-01-09 11:26:13 +0200

    Work for all browsers

--- timetable.js
+++ timetable.js
@@ -1,12 +1,13 @@
 // handle links to speeches
 $(function() {
-    $('td a').click(function() {
-        $('table').hide();
-        var file = this.id + ".json";
+    $('td a').click(function(e) {
         if (window.history && window.history.pushState) {
+            $('table').hide();
+            var file = this.id + ".json";
             var state = { "hidden": true };
             var title = "Speech: " + this.id;
             window.history.pushState(state, title);
+            e.preventDefault()
         }
     });
 });
