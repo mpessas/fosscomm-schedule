e1b05eb3491d5234e89cc4c665294889319f7557
Apostolos Bessas <mpessas@gmail.com>
2011-01-09 12:02:04 +0200

    Fetch data using json

--- timetable.js
+++ timetable.js
@@ -5,14 +5,31 @@ $(function() {
         if (window.history && window.history.pushState) {
             e.preventDefault()
             $('table').hide();
-            var file = this.id + ".json";
+            var filename = this.id + ".json";
             var state = { "hidden": true };
             var title = "Speech: " + this.id;
             window.history.pushState(state, title);
 
+            // load new data for selection
+            var path = filename;
+            alert(path);
+            $.ajax({
+                type: 'GET',
+                url: path,
+                timeout: 10000,
+                error: function(xhr, status) {
+                    alert(status);
+                },
+                success: function() {
+                    alert("SUCCESS");
+                }
+            });
+            $('div#res').show();
+
             // handle back button
             $(window).bind("popstate", function() {
                 $('table').show();
+                $('div#res').hide();
             });
         }
     });
