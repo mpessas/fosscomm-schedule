aa4f981e5182845428cad88526fe1279be793717
Apostolos Bessas <mpessas@gmail.com>
2011-01-09 18:57:46 +0200

    Split to two tables

--- timetable.js
+++ timetable.js
@@ -1,7 +1,16 @@
 function get_template(row, speech) {
-    row.find('.thour').html(speech.hour);
-    row.find('.tb1').text(speech.title);
-    row.find('.tb2').text(speech.speaker);
+    var speechHtml = '<a href="speech' + speech.id +
+        '.html" id="speech' + speech.id + '">' +
+        speech.title + '</a>';
+    if (speech.room == "Β1") {
+        row.find('.tb1hour').html(speech.hour);
+        row.find('.tb1speech').html(speechHtml);
+        row.find('.tb1speaker').text(speech.speaker);
+    } else if (speech.room == "Β4") {
+        row.find('.tb4hour').html(speech.hour);
+        row.find('.tb4speech').html(speechHtml);
+        row.find('.tb4speaker').text(speech.speaker);
+    }
     return row;
 }
 
@@ -16,16 +25,19 @@ $(function() {
             alert(status + ": " + error);
         },
         success: function(data) {
-            alert(data.length);
             for (var i in data) {
-                var newRow = $('.template').clone().removeClass('template');
-                alert(data[i]);
-                get_template(newRow, data[i]).appendTo('table');
+                if (data[i].room == "Β1") {
+                    var newRow = $('.b1template').clone().removeClass('b1template');
+                    get_template(newRow, data[i]).appendTo('table#schedule_b1');
+                } else if (data[i].room == "Β4") {
+                    var newRow = $('.b4template').clone().removeClass('b4template');
+                    get_template(newRow, data[i]).appendTo('table#schedule_b4');
+                }
             }
         }
     });
     // handle links to speeches
-    $('td a').click(function(e) {
+    $('td a').live('click', function(e) {
         // Support for js history API
         if (window.history && window.history.pushState) {
             e.preventDefault()
