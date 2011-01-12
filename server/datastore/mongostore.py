87a86cbdf9202dd01329f5693095b16b865d4afd
Apostolos Bessas <mpessas@gmail.com>
2011-01-12 11:33:05 +0200

    Pylint fixes, rename set to put

--- server/datastore/mongostore.py
+++ server/datastore/mongostore.py
@@ -1,33 +1,46 @@
 # -*- coding: utf-8 -*-
+"""Module for using MongoDB as datastore."""
 
 import pymongo
 
 
 class MongoStore(object):
+    """Class to interface with MongoDB."""
 
     def __init__(self):
-        self._connnection = None
+        self._connection = None
         self._db = None
 
-    def connect(self, db="fosscomm", host='localhost', port=27017):
+    def connect(self, dbname="fosscomm", host='localhost', port=27017):
+        """Connect to mongodb."""
         self._connection = pymongo.Connection(host, port)
-        self._db = self._connection[db]
+        self._db = self._connection[dbname]
 
     def disconnect(self):
+        """Disconnect from MongoDb.
+
+        It must be run, when the connection is no longer needed.
+        """
         self._connection.disconnect()
 
     def _get_collection(self):
+        """Return the desired collection of the database."""
         return self._db.presentations
 
-    def setup():
+    def setup(self):
         """Setup indices"""
         pass                    # TODO
 
-    def set(self, doc):
+    def put(self, doc):
+        """Add a new object to Datastore.
+
+        Argument must be a python dictionary.
+        """
         col = self._get_collection()
         return col.insert(doc)
 
     def get_mongo_id(self, mid):
+        """Get the object by the _id attribute used by MongoDb."""
         col = self._get_collection()
         return col.find_one({"_id": mid})
 
--- server/datastore/tests.py
+++ server/datastore/tests.py
@@ -24,18 +24,18 @@ class TestMongoStore(unittest.TestCase):
         self.store._connection.drop_database(self.dbname)
         self.store.disconnect()
 
-    def test_set(self):
+    def test_put(self):
         obj = ObjClass(1, 2)
-        self.assertTrue(self.store.set(obj.as_doc()))
-        self.assertRaises(TypeError, self.store.set, 4)
-        self.assertRaises(TypeError, self.store.set, [1, 2])
+        self.assertTrue(self.store.put(obj.as_doc()))
+        self.assertRaises(TypeError, self.store.put, 4)
+        self.assertRaises(TypeError, self.store.put, [1, 2])
 
     def test_get_mongo_id(self):
         res = self.store.get_mongo_id("123")
         self.assertTrue(res is None)
         obj = ObjClass(1, 2)
         doc = obj.as_doc()
-        mid = self.store.set(doc)
+        mid = self.store.put(doc)
         res = self.store.get_mongo_id(mid)
         self.assertEquals(res['x'], doc['x'])
         self.assertEquals(res['y'], doc['y'])
@@ -43,7 +43,7 @@ class TestMongoStore(unittest.TestCase):
     def test_get(self):
         obj = ObjClass(1, 2)
         doc = obj.as_doc()
-        mid = self.store.set(doc)
+        mid = self.store.put(doc)
         res = self.store.get('x', 1)
         self.assertEquals(res['_id'], mid)
         res = self.store.get('x', 4)
@@ -53,17 +53,17 @@ class TestMongoStore(unittest.TestCase):
         self.assertEquals(len([x for x in self.store.get_all()]), 0)
         obj1 = ObjClass(1, 2)
         obj2 = ObjClass(2, 4)
-        mid1 = self.store.set(obj1.as_doc())
-        mid2 = self.store.set(obj2.as_doc())
+        mid1 = self.store.put(obj1.as_doc())
+        mid2 = self.store.put(obj2.as_doc())
         self.assertEquals(len([x for x in self.store.get_all()]), 2)
 
     def test_filter(self):
         obj1 = ObjClass(1, 2)
         obj2 = ObjClass(2, 4)
         obj3 = ObjClass(2, 5)
-        mid1 = self.store.set(obj1.as_doc())
-        mid2 = self.store.set(obj2.as_doc())
-        mid3 = self.store.set(obj3.as_doc())
+        mid1 = self.store.put(obj1.as_doc())
+        mid2 = self.store.put(obj2.as_doc())
+        mid3 = self.store.put(obj3.as_doc())
         res = self.store.filter(x=2)
         self.assertEquals(len([x for x in res]), 2)
         for r in res:
@@ -77,7 +77,7 @@ class TestMongoStore(unittest.TestCase):
         self.assertEquals(res[0]['y'], 4)
         res = [x for x in self.store.filter(x=2, y=6)]
         self.assertEquals(len(res), 0)
-
+        
 
 if __name__ == '__main__':
     unittest.main()
