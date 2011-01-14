d086eed94e809f9c865c01dc54b0f1f3431ac86e
Apostolos Bessas <mpessas@gmail.com>
2011-01-14 17:19:06 +0200

    Create graph checkpoint

--- server/time_conflict/import.py
+++ server/time_conflict/import.py
@@ -16,16 +16,21 @@ def parse_cmd_options(argv):
     args = parser.parse_args(argv)
     return args
 
-def main(argv=None):
-    if argv is None:
-        argv = sys.argv[1:]
-    args = parse_cmd_options(argv)
-    filename = args.filename
+def get_events_from_file(filename):
     with open(filename) as f:
         events = json.load(f)
     event_node_list = []
     for e in events:
         event_node_list.append(graph.EventNode(e['id'], **e))
+    return event_node_list
+
+def main(argv=None):
+    if argv is None:
+        argv = sys.argv[1:]
+    args = parse_cmd_options(argv)
+    filename = args.filename
+    eventlist = get_events_from_file(filename)
+    graph.create_graph([e for e in eventlist if e.day==u"Σάββατο"])
 
 if __name__ == '__main__':
     sys.exit(main())
