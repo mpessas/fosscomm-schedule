41ea783a5e53f3f2539450d864fa35efd3fa053b
Apostolos Bessas <mpessas@gmail.com>
2011-01-15 11:35:18 +0200

    Update tests

--- server/time_conflict/tests.py
+++ server/time_conflict/tests.py
@@ -2,31 +2,31 @@
 # -*- coding: utf-8 -*-
 
 import unittest
-from graph import EventNode
+from graph import  Node, Event
 from graph import NotReadyError
 
 
-class TestEventNode(unittest.TestCase):
+class TestEvent(unittest.TestCase):
 
     def setUp(self):
-        self.node = EventNode(3)
-        self.attrs = self.node._EventNode__attrs.iterkeys()
+        self.node = Node(Event(3))
+        self.attrs = self.node._wraps._Event__attrs.iterkeys()
 
     def test_initializer(self):
-        node = EventNode(3, title=u"title", summary=u"summary")
+        node = Node(Event(3, title=u"title", summary=u"summary"))
         self.assertEquals(node.title, u"title")
         self.assertEquals(node._id, 3)
         self.assertEquals(node.summary, u"summary")
         self.assertTrue(node.speaker is None)
-        self.assertRaises(AttributeError, EventNode, 3, t=u"title")
+        self.assertRaises(AttributeError, Event, 3, t=u"title")
 
     def test_ready(self):
-        """Check that EventNode._is_ready returns True,
+        """Check that Event._is_ready returns True,
         iff all fields are set.
         """
         for attr in self.attrs:
             self.assertFalse(self.node._is_ready())
-            setattr(self.node, attr, "val")
+            setattr(self.node._wraps, attr, "val")
         self.assertTrue(self.node._is_ready())
 
     def test_wrong_attribute(self):
@@ -40,11 +40,11 @@ class TestEventNode(unittest.TestCase):
 
     def test_start_time(self):
         time = "10:30"
-        self.node.time_end = time
+        self.node._wraps.time_end = time
         self.assertEquals(self.node.ends(), time)
 
     def test_add_conflicts(self):
-        e = EventNode(5)
+        e = Node(Event(5))
         self.node.add_conflict_with_node(e)
         self.assertTrue(e.get_id() in self.node.conflicts_with)
 
